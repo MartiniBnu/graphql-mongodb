032317af97007a0b0bb5ce44bc2818f0e676b8fd
Marcos Martini <martini.bnu@gmail.com>
2022-01-13 13:24:07 -0300

    End of the course, it was amazinggit add .!

--- src/lesson/lesson.input.ts
+++ src/lesson/lesson.input.ts
@@ -1,15 +1,22 @@
-import { Field, InputType } from "@nestjs/graphql";
-import { MinLength, IsDateString } from "class-validator";
+import { Field, ID, InputType } from "@nestjs/graphql";
+import { MinLength, IsDateString, IsUUID } from "class-validator";
 
 @InputType()
 export class CreateLessonInput {
+
   @MinLength(1)
   @Field()
   name: string;
+
   @IsDateString()
   @Field()
   startDate: string;
+
   @IsDateString()
   @Field()
   endDate: string;
+
+  @IsUUID("all", {each: true})
+  @Field(() => [ID], { defaultValue: []})
+  students: string[];
 }
